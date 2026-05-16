61ba032157514f9c8b2abb0ae1ef777e7cde8330
Cacti <195132894+NoPrShub@users.noreply.github.com>
2026-05-16 20:38:29 +0530

    add getComputerChoice() function
    returns the computer's choice: rock or paper or scissors

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// randomNumber(): returns random number between 1 and 3
+// randomNumber(): returns random number between 1 and 3 inclusive
 function randomNumber() {
     return Math.floor(Math.random() * 3) + 1;
 }
-console.log(randomNumber());
+
+// getComputerChoice(): returns computer choice
+function getComputerChoice() {
+    let randNum = randomNumber();
+    let guess;
+
+    switch(randNum) {
+        case 1:
+            guess = "rock";
+            break;
+        case 2:
+            guess = "paper";
+            break;
+        case 3:
+            guess = "scissors";
+            break;
+    }
+    return guess;
+}
